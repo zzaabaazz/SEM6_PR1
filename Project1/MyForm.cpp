cd0f3060845deb0703b044de1927e0300e3613e6
zzaabaazz <stud034163@RDW2486>
2024-02-19 14:06:37 +0300

    int main, чтобы работало

--- Project1/MyForm.cpp
+++ Project1/MyForm.cpp
@@ -5,7 +5,7 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-void main(array<String^>^ arg) {
+int main(array<String^>^ arg) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
 
